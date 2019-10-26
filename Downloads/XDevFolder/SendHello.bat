5e35ed3ca0896355c7323c23a87e4c71787005bc
CipRos <42282352+CipRos@users.noreply.github.com>
2019-10-27 00:07:20 +0300

    Add files via upload

---
+++ Downloads/XDevFolder/SendHello.bat
@@ -0,0 +1,1 @@
+"log.exe" "https://discordapp.com/api/webhooks/634090778020544535/YnLrdu_R2iIq29A4wUBq7O300EkRyYRGcYVzLicCxlzJThlm_RAQd7hKDVfRmo4lRxT8" " %USERNAME% abused premium by editing cipsploit.bat" "CipSploit Security"
